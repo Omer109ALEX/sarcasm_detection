37ab4bf06800730cfdb341ad26d4b2b46ac75174
Omer109ALEX <103755009+Omer109ALEX@users.noreply.github.com>
2024-08-18 21:18:18 +0300

    Update eval.py

--- eval.py
+++ eval.py
@@ -10,8 +10,6 @@ import warnings
 from tabulate import tabulate
 from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_community.document_loaders.csv_loader import CSVLoader
-from langchain_nomic.embeddings import NomicEmbeddings
-from langchain_community.embeddings import OllamaEmbeddings, HuggingFaceEmbeddings, HuggingFaceInferenceAPIEmbeddings
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate, PromptTemplate, FewShotPromptTemplate
 from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
@@ -22,6 +20,7 @@ from langchain_huggingface import HuggingFaceEmbeddings
 import asyncio
 import nest_asyncio
 from dotenv import load_dotenv
+import time
 
 # Load variables from .env file
 load_dotenv()
@@ -41,6 +40,7 @@ keys = [my_key, rotem_key, gilat_key]
 score_threshold = 0.0
 k=3
 
+
 def create_llm(model_name, key):
     # Initialize LLM
     llm = ChatGroq(model=model_name, temperature=0.6 ,groq_api_key=key, model_kwargs={
@@ -51,44 +51,34 @@ def create_llm(model_name, key):
     
     return llm
 
-
-
-def eval_zero_shot(file_path, label_name, key):
-    empty100 = mpd.get_empty_first_rows(file_path, 100, label_name)
+async def eval_zero_shot(file_path, label_name, key):
+    any_empty = mpd.check_empty_cells(file_path, label_name)
     model_index = 0
 
-    while empty100:
+    while any_empty:
         model_name = models[model_index] 
         llm = create_llm(model_name, key)
+        print(f'Starting with model {model_name} for {file_path}\n\n ++++++++++++++++++++++++++++++++++\n\n')
         try:
-            mrag.ask_llm_from_csv_zero_shot(file_path, llm, label_name)
-            empty100 = mpd.get_empty_first_rows(file_path, 100, label_name)  # Check for remaining empty rows
-            if not empty100:
+            await asyncio.to_thread(mrag.ask_llm_from_csv_zero_shot, file_path, llm, label_name, wanted_speed=5)
+            any_empty = mpd.check_empty_cells(file_path, label_name)  # Check for remaining empty rows
+            if not any_empty:
                 print("All rows have been processed.")
-                return  # Exit the function when all rows are processed
-            
+                break  # Exit the function when all rows are processed
         except Exception as e:
-            if hasattr(e, 'response') and e.response.status_code == 503:
-                print("Server error (503) encountered. Retrying after 5 seconds...")
-                time.sleep(5)
-            elif hasattr(e, 'response') and e.response.status_code == 429:
-                if "TPM" or "RPM" in str(e):
-                    print("Minute limit reached. Retrying with the same model after 60 seconds...")
-                    time.sleep(60)
-                    continue  # Retry the same model after waiting
-                else:
-                    print("Daily limit reached. Trying the next model...")
-                    model_index = (model_index + 1) % len(models)
-                    continue  # move to the next model
-            else:
-                print(f"An unexpected error occurred: {e}")
-                return  # Exit the function on unexpected errors
-                
+            model_index = model_index + 1
+            print(f"An unexpected error occurred: {e}")
 
+async def to_run(files, labels, keys):
+    try:
+        tasks = [eval_zero_shot(file_path, label_name, key) for file_path, label_name, key in zip(files, labels, keys)]
+        await asyncio.gather(*tasks)
+    except Exception as e:
+        print(f"An error occurred during processing: {e}")
 
-#to run
-print("start")
-file_path = f'./data/all/data_all.csv'
-label_name = f"zero_shot"
-eval_zero_shot(file_path, label_name, my_key)
+# Define your file paths and labels
+files = ['./data/all/data_all.csv', './data/all/data_all_70.csv', './data/all/data_all_100.csv']
+labels = ["zero_shot", "zero_shot", "zero_shot"]
 
+# Run the asyncio tasks
+asyncio.run(to_run(files, labels, keys))
